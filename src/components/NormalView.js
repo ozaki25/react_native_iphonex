62d6227d2b4579590f8ae869ba39b0a88ea60f95
ozaki <fs65ugumori@yahoo.co.jp>
2018-02-16 01:03:15 +0900

    add dialog

--- src/components/NormalView.js
+++ src/components/NormalView.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import {
+  Button,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Dialog } from 'react-native-simple-dialogs';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,11 +25,28 @@ const styles = StyleSheet.create({
 const NormalView = ({ navigation }) => (
   <View style={styles.container}>
     <TouchableOpacity style={styles.contents} onPress={() => navigation.navigate('iphonex')}>
-      <Text>
-        NormalView
-        <Icon name="md-alert" size={30} color="#900" />
-      </Text>
+      <Text>NormalView</Text>
     </TouchableOpacity>
+
+    <Dialog
+      title="タイトル"
+      buttons={<Button title="ボタン" color="blue" onPress={() => {}} />}
+      overlayStyle={{ paddingHorizontal: 10 }}
+      contentStyle={{
+        backgroundColor: 'lightblue',
+        height: Dimensions.get('window').height - 150,
+        paddingHorizontal: 10,
+      }}
+      visible
+    >
+      <ScrollView>
+        {[...Array(3)].map((_, i) => (
+          <Text key={i}>
+            ダイアログダイアログダイアログダイアログダイアログダイアログダイアログ
+          </Text>
+        ))}
+      </ScrollView>
+    </Dialog>
   </View>
 );
 
